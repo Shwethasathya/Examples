56bbba08c35115cf35c605592ebdf2867ebecef1
sshwetha.xworkz@gmail.com <sshwetha.xworkz@gmail.com>
2022-11-14 09:00:31 +0530

    Basic count programs

---
+++ coading/src/com/xworkz/basicprograms/programs/Count.java
@@ -0,0 +1,21 @@
+
+//count number of vowels and consonents in the string
+
+package com.xworkz.basicprograms.programs;
+
+public class Count {
+
+	public static void main(String[] args) {
+
+		String str = "Mobile is a android";
+		int vowelsCount = 0;
+		int consonenetsCounts = 0;
+
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o'
+					|| str.charAt(i) == 'u')
+
+				vowelsCount++;
+		}
+	}
+}
