c3dd253e661dde0b21aa967ef462c891118ed3cb
Shwethasathya <113284867+Shwethasathya@users.noreply.github.com>
2022-11-16 23:19:04 +0530

    Add files via upload

---
+++ coading/src/com/xworkz/basicprograms/programs/Addition.java
@@ -0,0 +1,14 @@
+class Addition{
+
+public int sum(int a, int b)
+	{
+		
+		for(int i=1; i<=b; i++)
+		{
+		a++;
+		return a;		
+		}
+		
+		return 0;
+	}
+}
---
+++ coading/src/com/xworkz/basicprograms/programs/AdditionRunner.java
@@ -0,0 +1,12 @@
+class AdditionRunner{
+
+public static void main(String[] args) {
+		
+		int sum = sum(2,4);
+		
+		System.out.println(sum);
+}
+}
+
+
+
